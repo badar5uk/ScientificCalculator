31d3b46c659424cfc73635a764aadb9b58140e09
bader <bader5uk@hotmail.com>
2024-11-18 08:22:48 +0400

    Added final velocity squared calculator service

---
+++ Services/FinalVelocitySquaredCalculator.java
@@ -0,0 +1,34 @@
+/*
+    This service is used to calculate the final velocity of an object.
+    A scanner is used to take input for the Initial Velocity, Acceleration and time.
+    The final velocity is calculated using v = u + at
+    u = initial velocity
+    a = acceleration
+    t = time
+ */
+
+package ScientificCalculator.Services;
+import ScientificCalculator.Entities.MotionData;
+import ScientificCalculator.Interfaces.CalculatorInterface;
+
+import java.util.Scanner;
+
+public class FinalVelocitySquaredCalculator implements CalculatorInterface{
+
+    static Scanner scanner = new Scanner(System.in);
+
+    @Override
+    public void calculate() {
+        Scanner scanner = new Scanner(System.in);
+        MotionData motion= new MotionData();
+        System.out.println("Enter initial Velocity: ");
+        Float u = scanner.nextFloat();
+        motion.setInitialVelocity(u);
+        System.out.println("Enter Acceleration: ");
+        Float a = scanner.nextFloat();
+        motion.setAcceleration(a);
+        System.out.println("Enter time: ");
+        Float t = scanner.nextFloat();
+        motion.setTime(t);
+    }
+}
