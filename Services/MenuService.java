5bec89bcc4acdc4de2c8f14a3d8490811c1b94fc
bader <bader5uk@hotmail.com>
2024-11-18 21:00:39 +0400

    Added comments and used a close() method to close scanner input

--- Services/MenuService.java
+++ Services/MenuService.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 
 public class MenuService {
 
-    static Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);// initiate scanner for menu input
 
     public static void mainMenu() {
-        Boolean exitFlag = true;
+        System.out.println("\n"+"welcome to my scientific Calculator app" + "\n" +
+                "Please pick an option: \n");
+        Boolean exitFlag = true; // exit flag to end program if exit option is picked
         while (exitFlag) {
-            System.out.println("welcome to my scientific Calculator app" + "\n" +
-                    "Please pick an option: \n" +
+            System.out.println(
                     "1. Final Velocity (v = u + at)\n" +
                     "2. Displacement (s = ut + ½at²)\n" +
                     "3. Final Velocity Squared (v² = u² + 2as)\n" +
@@ -23,30 +24,41 @@ public class MenuService {
             try {
                 Integer mainMenuInput = scanner.nextInt();
                 if (mainMenuInput.equals(1)) {
+                    // if first option is chosen, the Final velocity calculator is called
                     FinalVelocityCalculator velocity = new FinalVelocityCalculator();
                     velocity.calculate();
                 } else if (mainMenuInput.equals(2)) {
+                    // if second option is chosen, Displacement Calculator is called
                     DisplacementCalculator displacement = new DisplacementCalculator();
                     displacement.calculate();
                 } else if (mainMenuInput.equals(3)) {
+                    // if third option is chosen, Final velocity squared is called
                     FinalVelocitySquaredCalculator velocitySquared = new FinalVelocitySquaredCalculator();
                     velocitySquared.calculate();
                 } else if (mainMenuInput.equals(4)) {
+                    // if fourth option is chosen, Area of circle is called
                     AreaofCircleCalculator Area = new AreaofCircleCalculator();
                     Area.calculate();
                 } else if (mainMenuInput.equals(5)) {
+                    // if fifth option is chosen, simple interest calculator is called
                     SimpleinterestCalculator simpleInterest = new SimpleinterestCalculator();
                     simpleInterest.calculate();
                 } else if (mainMenuInput.equals(6)) {
+                    // if sixth option is chosen, Force calculator is called
                     ForceCalculator force = new ForceCalculator();
                     force.calculate();
                 } else if (mainMenuInput.equals(7)) {
+                    // if seventh option is chosen, Density calculator is called
                     DensityCalculator density = new DensityCalculator();
                     density.calculate();
                 } else if (mainMenuInput.equals(8)) {
+                    // if eighth option is chosen, Compound interest calculator is called
                     CompoundInterest compoundInterest = new CompoundInterest();
                     compoundInterest.calculate();
                 } else if (mainMenuInput.equals(9)) {
+                    // if ninth option is chosen, scanner input is closed and it exits
+                    System.out.println("Good Bye");
+                    scanner.close();
                     exitFlag = false;
                 }
             } catch (Exception e) {
